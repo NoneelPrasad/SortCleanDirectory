0e65537e836f56553715800f6a05ec6651c68bbd
Noneel Prasad <prasad_no@usp.ac.fj>
2021-02-05 14:32:52 +1200

    passing argument of path

---
+++ cleanup.py
@@ -0,0 +1,20 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Clean up directory and put files into according folders."
+)
+
+# passs path as --path when runniong from cmd
+
+parser.add_argument(
+    "--path",
+    type=str,
+    default=".",
+    help="Directory path of the to be cleaned directory",
+)
+# parse the arguments given by the user and extract the path
+args = parser.parse_args()
+path = args.path
+
+print(f"Cleaning up directory {path}")
